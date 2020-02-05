daec9a1a34eeb54aab35bd774fd10fd415ab58e2
Arivlorn <60198052+Arivlorn@users.noreply.github.com>
2020-02-05 15:54:40 -0500

    Initial commit. If you screw it all up, this'll be the place you go back to. :T

---
+++ app/src/main/java/edu/andrews/cptr252/aisensee/bugtracker/BugTrackerActivity.java
@@ -0,0 +1,14 @@
+package edu.andrews.cptr252.aisensee.bugtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BugTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bug_tracker);
+    }
+}
